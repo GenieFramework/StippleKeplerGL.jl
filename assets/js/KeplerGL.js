7157536ce030ff22c16bf010dfe53ce409087ff8
Helmut Hänsel <helmut.haensel@gmx.de>
2024-04-05 03:14:49 +0300

    first attempts, no module yet

---
+++ assets/js/KeplerGL.js
@@ -0,0 +1,168 @@
+Vue.component("KeplerGl", {
+    template: '<div id="id1"></div>',
+    inheritAttrs: !1,
+    props: {
+        id: {
+            type: String,
+            required: !1,
+            default: 'kepler-gl'
+        },
+        map: {
+            type: Object
+        },
+    },
+    data() {
+        return {
+            
+        }
+    },
+    mounted() {
+        this.replot(this.map)
+    },
+    methods: {
+        make_initial_state(m) {
+            return KeplerGl.keplerGlReducer.initialState({
+                uiState: {
+                    readOnly: m.window.read_only,
+                    activeSidePanel: null, 
+                    currentModal: null,
+                    mapControls: {
+                        visibleLayers: {
+                            show: m.window.visible_layers_show,
+                            active: m.window.visible_layers_active
+                        },
+                        mapLegend: {
+                            show: m.window.map_legend_show,
+                            active: m.window.map_legend_active
+                        },
+                        toggle3d: {
+                            show: m.window.toggle_3d_show,
+                            active: m.window.toggle_3d_active
+                        },
+                        splitMap: {
+                            show: m.window.split_map_show,
+                            active: m.window.split_map_active
+                        }
+                    }
+                }
+            })
+        },
+        replot(m) {
+            /* Validate Mapbox Token */
+            MAPBOX_TOKEN = m.window.token
+            console.log(MAPBOX_TOKEN)
+            if ((MAPBOX_TOKEN || '') === '' || MAPBOX_TOKEN === 'PROVIDE_MAPBOX_TOKEN') {
+            alert(WARNING_MESSAGE);
+            }
+
+            /** STORE **/
+            state = this.make_initial_state(m)
+            const reducers = (function createReducers(Redux, KeplerGl) {
+            return Redux.combineReducers({
+                // mount KeplerGl reducer
+                keplerGl: state
+            });
+            }(Redux, KeplerGl));
+
+            const middleWares = (function createMiddlewares(KeplerGl) {
+            return KeplerGl.enhanceReduxMiddleware([
+                // Add other middlewares here
+            ]);
+            }(KeplerGl));
+
+            const enhancers = (function createEnhancers(Redux, middles) {
+            return Redux.applyMiddleware(...middles);
+            }(Redux, middleWares));
+
+            const store = (function createStore(Redux, enhancers) {
+            const initialState = {};
+
+            return Redux.createStore(
+                reducers,
+                initialState,
+                Redux.compose(enhancers)
+            );
+            }(Redux, enhancers));
+            // expose store globally:
+            window.store = store;
+            /** END STORE **/
+
+            /** COMPONENTS **/
+            const KeplerElement = (function (react, KeplerGl, mapboxToken) {
+                return function(props) {
+                    return react.createElement(
+                    'div',
+                    {style: {position: 'relative', left: 0, width: '100%', height: '100%'}},
+                    react.createElement(
+                        KeplerGl.KeplerGl,
+                        {
+                            mapboxApiAccessToken: mapboxToken,
+                            id: this.id,
+                            width: props.width || m.window.width,
+                            height: props.height || m.window.height
+                        }
+                    )
+                    )
+                }
+            }(React, KeplerGl, MAPBOX_TOKEN));
+
+            const app = (function createReactReduxProvider(react, ReactRedux, KeplerElement) {
+                return react.createElement(
+                    ReactRedux.Provider,
+                    {store},
+                    react.createElement(KeplerElement, null)
+                )
+            }(React, ReactRedux, KeplerElement));
+            /** END COMPONENTS **/
+
+            /** Render **/
+            (function render(react, ReactDOM, app) {
+            ReactDOM.render(app, document.getElementById('id1'));
+            }(React, ReactDOM, app));
+
+            const newDatasets = [];
+            m.datasets.forEach( (d) => {
+                if (d.hasOwnProperty('datasets')) {
+                    keplergljson = {
+                        datasets: [d],
+                        config: m.config
+                    }
+                    processeddata = [KeplerGl.processKeplerglJSON(keplergljson).datasets[0].data]
+                } else if (d.hasOwnProperty('csvstring')) {
+                    processeddata = [KeplerGl.processCsvData(d.csvstring)]
+                } else  if (d.hasOwnProperty('json')) {
+                    processeddata = [KeplerGl.processGeojson(d.json)]            
+                }
+
+                // for debugging
+                // console.log(processeddata)
+
+                // match id with old datasets
+                newDataset = processeddata.map((d, i) => ({
+                version: m.config.version,
+                data: {
+                    id: d.id,
+                    label: d.id,
+                    allData: d.rows,
+                    fields: d.fields
+                }
+                }));
+
+                newDatasets.push(newDataset[0]);
+                
+                // for debugging
+                // console.log(newDataset)
+            })
+        
+            // const config = m.config
+        
+            const loadedData = KeplerGl.KeplerGlSchema.load(
+                newDatasets,
+                m.config
+            );
+        
+            loadedData['options'] = {centerMap: m.window.center_map, readOnly: m.window.read_only};
+            store.dispatch(KeplerGl.addDataToMap(loadedData));
+        }
+    }
+});
---
+++ src/StippleKeplerGL.jl
@@ -0,0 +1,66 @@
+using Stipple, Stipple.ReactiveTools
+using StippleUI
+using KeplerGL
+using DataFrames
+using CSV
+using Colors
+using ColorBrewer
+
+df = CSV.read("../packages/KeplerGL/V6OzT/assets/example_data/data.csv", DataFrame)
+
+token = "token please"   
+
+m = KeplerGL.KeplerGLMap(token, center_map=false)
+KeplerGL.add_point_layer!(m, df, :Latitude, :Longitude,
+    color = colorant"rgb(23,184,190)", color_field = :Magnitude, color_scale = "quantize", 
+    color_range = ColorBrewer.palette("PRGn", 6),
+    radius_field = :Magnitude, radius_scale = "sqrt", radius_range = [4.2, 96.2], radius_fixed = false,
+    filled = true, opacity = 0.39, outline = false);
+
+m.config[:config][:mapState][:latitude] = 38.32068477880718
+m.config[:config][:mapState][:longitude]= -120.42806781055732
+m.config[:config][:mapState][:zoom] = 4.886825331541375
+m.window[:map_legend_show] = false
+m.window[:map_legend_active] = false
+m.window[:visible_layers_show] = false
+m.window[:visible_layers_active] = false
+
+@app HH begin
+    @out map = m
+end
+
+keplergl_assets_config = Genie.Assets.AssetsConfig(package = "KeplerGL.jl")
+assets_config = Genie.Assets.AssetsConfig(package = "StippleKeplerGL.jl")
+
+basedir = dirname(dirname(pathof(KeplerGL)))
+
+deps_routes = String[]
+for js in [
+    "react.production.min.js", "react-dom.production.min.js",
+    "redux.js", "react-redux.min.js",
+    "styled-components.min.js", "keplergl.min.js",
+  ]
+  s = script(src = Genie.Assets.add_fileroute(keplergl_assets_config, js; basedir).path)
+  push!(deps_routes, s)
+end
+
+basedir = "C:/Users/helmu/.julia/dev/StippleKeplerGL"
+s = script(src = Genie.Assets.add_fileroute(assets_config, "KeplerGL.js"; basedir).path)
+push!(deps_routes, s)
+
+kepler_deps() = deps_routes
+
+@deps HH kepler_deps
+
+ui() = [
+    h1("Hello World")
+    xelem(R"kepler-gl", map = :map)
+]
+
+route("/") do
+    global model
+    model = @init HH
+    page(model, ui) |> html
+end
+
+up(open_browser = true)
